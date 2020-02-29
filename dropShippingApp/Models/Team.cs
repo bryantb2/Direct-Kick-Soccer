37491cd186075b9e8d3e524d93b0363dc98b52de
Blake <bryantb2@my.lanecc.edu>
2020-02-28 20:32:10 -0800

    added team tags, team, and proprietary collection

--- dropShippingApp/Models/Team.cs
+++ dropShippingApp/Models/Team.cs
@@ -7,5 +7,54 @@ namespace dropShippingApp.Models
 {
     public class Team
     {
+        // private fields
+        private List<CustomProduct> teamProducts = new List<CustomProduct>();
+        private List<TeamTag> teamTags = new List<TeamTag>();
+
+        // public properties
+        public int TeamID { get; set; }
+        public String TeamName { get; set; }
+        public String TeamDescription { get; set; }
+        public Country Country { get; set; }
+        public String StreetAddress { get; set; }
+        public int ZipCode { get; set; }
+        public String CorporatePageURL { get; set; }
+        public String BusinessEmail { get; set; }
+        public int PhoneNumber { get; set; }
+        public List<CustomProduct> TeamProducts { get { return teamProducts; } }
+        public List<TeamTag> TeamTags { get { return teamTags; } }
+
+        // methods
+        public void AddTag(TeamTag tag) => teamTags.Add(tag);
+        public TeamTag RemoveTag(TeamTag tag)
+        {
+            TeamTag removedTag = null;
+            foreach (TeamTag t in teamTags)
+            {
+                if (t.TeamTagID == tag.TeamTagID)
+                {
+                    removedTag = t;
+                    teamTags.Remove(t);
+                    return removedTag;
+                }
+            }
+            return removedTag;
+        }
+
+        public void AddProduct(CustomProduct product) => teamProducts.Add(product);
+        public CustomProduct RemoveProduct(CustomProduct product)
+        {
+            CustomProduct removedProduct = null;
+            foreach (CustomProduct p in teamProducts)
+            {
+                if (product.CustomProductID == p.CustomProductID)
+                {
+                    removedProduct = p;
+                    teamProducts.Remove(p);
+                    return removedProduct;
+                }
+            }
+            return removedProduct;
+        }
     }
 }
