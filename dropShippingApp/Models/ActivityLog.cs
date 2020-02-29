cddf7349416445628fa089dff6d9f0b0b23b43db
Blake <bryantb2@my.lanecc.edu>
2020-02-28 20:03:06 -0800

    added custom product and pricing history model

--- dropShippingApp/Models/ActivityLog.cs
+++ dropShippingApp/Models/ActivityLog.cs
@@ -7,5 +7,16 @@ namespace dropShippingApp.Models
 {
     public class ActivityLog
     {
+        // public properties
+        public int ActivityLogID { get; set; }
+        public String Title { get; set; } // example: "Added product" 
+        public String ChangeDescription { get; set; } // example: "red sweatshirt with small, medium, and large sizes"
+        public DateTime TimeStamp { get; set; } 
+
+        // methods
+        public String LogToString()
+        {
+            return (Title + ", " + ChangeDescription + " on " + TimeStamp.ToString());
+        }
     }
 }
