6714b53b9bd2afa909d75a5bb63a5054e262c2a8
bryantb2 <bryantb2@my.lanecc.edu>
2020-03-02 13:50:41 -0800

    removed error models

--- dropShippingApp/Startup.cs
+++ dropShippingApp/Startup.cs
@@ -42,11 +42,11 @@ namespace dropShippingApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                //app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
