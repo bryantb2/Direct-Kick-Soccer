6031d05605bf831276771ed3f9e5c8bc63875d86
bryantb2 <bryantb2@my.lanecc.edu>
2020-03-02 13:13:32 -0800

    added controller classes and views for client side site

--- dropShippingApp/Controllers/HomeController.cs
+++ dropShippingApp/Controllers/HomeController.cs
@@ -11,12 +11,6 @@ namespace dropShippingApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
@@ -28,10 +22,5 @@ namespace dropShippingApp.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
