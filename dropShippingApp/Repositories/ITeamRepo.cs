30784eb29c3d78e88e2469609dd98340cf05d4e7
bryantb2 <bryantb2@my.lanecc.edu>
2020-03-09 19:05:17 -0700

    repo sync

--- dropShippingApp/Repositories/ITeamRepo.cs
+++ dropShippingApp/Repositories/ITeamRepo.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dropShippingApp.Models;
 
 namespace dropShippingApp.Repositories
 {
-    public class ITeamRepo
+    public interface ITeamRepo
     {
+        Task<Team> AddTeamAsync(Team team);
+
+        Task<int> RemoveTeamAsync(int teamId);
+
+        Task<Team> UpdateTeamAsync(Team team);
     }
 }
--- dropShippingApp/Repositories/TeamRepo.cs
+++ dropShippingApp/Repositories/TeamRepo.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using dropShippingApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace dropShippingApp.Repositories
 {
-    public class TeamRepo
+    public class TeamRepo : ITeamRepo
     {
+        private List<Team> teams = new List<Team>();
+        public async Task<Team> AddTeamAsync(Team team)
+        {
+            return team;
+        }
+
+        public async Task<int> RemoveTeamAsync(int teamId)
+        {
+            var foundTeam = teams.Find(team => team.TeamID == teamId);
+            teams.Remove(foundTeam);
+            return foundTeam;
+        }
+
+        public async Task<Team> UpdateTeamAsync(Team team)
+        {
+
+        }
     }
 }
