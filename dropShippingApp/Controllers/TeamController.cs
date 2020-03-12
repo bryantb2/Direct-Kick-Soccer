5129e70520fa1dbae2311a079f8861ebea0551fd
Blake <bryantb2@my.lanecc.edu>
2020-03-11 18:08:48 -0700

    added action methods to team controller

--- dropShippingApp/Controllers/TeamController.cs
+++ dropShippingApp/Controllers/TeamController.cs
@@ -22,56 +22,15 @@ namespace dropShippingApp.Controllers
 
         public async Task<IActionResult> BrowseTeams()
         {
-
+            // TODO
+            // returns team results page 
+            return View();
         }
 
         public async Task<IActionResult> SearchTeams(string searchTerm)
         {
             // TODO
-            // returns team results page
-            return View();
-        }
-
-        public async Task<IActionResult> ManageTeamProducts()
-        {
-            // TODO
-            // returns team product management page
-            return View();
-        }
-
-        public async Task<IActionResult> AddTeamProduct()
-        {
-            // TODO
-            // redirects to team product management page
-            return View();
-        }
-
-        public async Task<IActionResult> UpdateTeamProduct()
-        {
-            // TODO
-            // redirects to team product management page
-            return View();
-        }
-
-        public async Task<IActionResult> RemoveTeamProduct()
-        {
-            // TODO
-            // redirects to team product management page
-            return View();
-        }
-
-        public async Task<IActionResult> UpdateTeamSettings()
-        {
-            // TODO: will take in settings view model
-            // redirect to home management page
-            return View();
-        }
-
-        public async Task<IActionResult> UploadNewBanner()
-        {
-            // TODO: will take in formdata with an image
-            // shove image into AWS file system
-            // return home management page
+            // returns team results page (will have view model with search term)
             return View();
         }
     }
--- dropShippingApp/Controllers/TeamManagementController.cs
+++ dropShippingApp/Controllers/TeamManagementController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using dropShippingApp.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,53 @@ namespace dropShippingApp.Controllers
 {
     public class TeamManagementController
     {
+        ITeamRepo teamRepo;
+        public TeamManagementController(ITeamRepo t)
+        {
+            teamRepo = t;
+        }
+
+        public async Task<IActionResult> ManageTeamProducts()
+        {
+            // TODO
+            // returns team product management page
+            return View();
+        }
+
+        public async Task<IActionResult> AddTeamProduct()
+        {
+            // TODO
+            // redirects to team product management page
+            return View();
+        }
+
+        public async Task<IActionResult> UpdateTeamProduct()
+        {
+            // TODO
+            // redirects to team product management page
+            return View();
+        }
+
+        public async Task<IActionResult> RemoveTeamProduct()
+        {
+            // TODO
+            // redirects to team product management page
+            return View();
+        }
+
+        public async Task<IActionResult> UpdateTeamSettings()
+        {
+            // TODO: will take in settings view model
+            // redirect to home management page
+            return View();
+        }
+
+        public async Task<IActionResult> UploadNewBanner()
+        {
+            // TODO: will take in formdata with an image
+            // shove image into AWS file system
+            // return home management page
+            return View();
+        }
     }
 }
