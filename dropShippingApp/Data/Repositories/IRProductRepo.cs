4d3b23ab4ea993f28b768f03f7a3bb4dece0f924
Blake <bryantb2@my.lanecc.edu>
2020-04-07 14:49:44 -0700

    added roster product interface

---
+++ dropShippingApp/Data/Repositories/IRProductRepo.cs
@@ -0,0 +1,16 @@
+﻿using dropShippingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dropShippingApp.Data.Repositories
+{
+    public interface IRProductRepo
+    {
+        public bool RosterProdExists(int id);
+        public Task<int> AddRosterProdAsync(RosterProduct rp);
+        public Task<RosterProduct> RemoveRosterProdAsync(int? id);
+        public Task<IQueryable<RosterProduct>> GetAllRosterProdAsync();
+    }
+}
