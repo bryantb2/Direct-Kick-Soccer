eeee7ecdcc35fd93832682028c90aa6220b54761
Blake <bryantb2@my.lanecc.edu>
2020-04-08 15:34:35 -0700

    added DbSet and removed old email notification model

--- dropShippingApp/Data/ApplicationDbContext.cs
+++ dropShippingApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using dropShippingApp.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +9,22 @@ namespace dropShippingApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<ActivityLog> ActivityLogs { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Providence> Providences { get; set; }
+        public DbSet<CustomProduct> CustomProducts { get; set; }
+        public DbSet<RosterProduct> RosterProducts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<PricingHistory> PricingHistories { get; set; }
+        public DbSet<ProductColor> ProductColors { get; set; }
+        public DbSet<ProductSize> ProductSizes { get; set; }
+        public DbSet<ProductTag> ProductTags { get; set; }
+        public DbSet<ProprietaryCollection> ProprietaryCollections { get; set; }
+        public DbSet<QuestionMessage> QuestionMessages { get; set; }
+        public DbSet<QuestionResponse> QuestionResponses { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<TeamCreationRequest> TeamCreationRequests { get; set; }
+        public DbSet<TeamTag> TeamTags { get; set; }
     }
 }
