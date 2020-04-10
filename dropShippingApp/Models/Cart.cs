45c9effebcf7ba6410f0f52e92600f311c27846e
Blake <bryantb2@my.lanecc.edu>
2020-04-09 17:48:16 -0700

    added cart item and cart models

---
+++ dropShippingApp/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dropShippingApp.Models
+{
+    public class Cart
+    {
+        public int CartID { get; set; }
+        public int SessionID { get; set; } // optional for guest users that do not have accounts
+        public List<CartItem> CartItems { get; set; }
+    }
+}
---
+++ dropShippingApp/Models/CartItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dropShippingApp.Models
+{
+    public class CartItem
+    {
+        public int CartItemID { get; set; }
+        public CustomProduct ProductSelection { get; set; }
+        public int Quantity { get; set; }
+    }
+}
