2956af923c1adc8f86b3587e99074b699881aa15
Blake <bryantb2@my.lanecc.edu>
2020-04-14 14:36:58 -0700

    working on cart and app user repos

---
+++ dropShippingApp/Data/Repositories/ICartRepo.cs
@@ -0,0 +1,15 @@
+﻿using dropShippingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dropShippingApp.Data.Repositories
+{
+    public interface ICartRepo
+    {
+        Task UpdateCart(Cart cart);
+        List<Cart> GetCarts { get; }
+        Task<Cart> FindCartById(int cartId);
+    }
+}
---
+++ dropShippingApp/Data/Repositories/RealRepos/RealCartRepo.cs
@@ -0,0 +1,91 @@
+﻿using dropShippingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dropShippingApp.Data.Repositories.RealRepos
+{
+    public class RealCartRepo : ICartRepo
+    {
+        private ApplicationDbContext context;
+        public RealCartRepo(ApplicationDbContext c)
+        {
+            this.context = c;
+        }
+
+        public async Task UpdateCart(Cart cart)
+        {
+            this.context.Carts.Update(cart);
+            await this.context.SaveChangesAsync();
+        }
+
+        public List<Cart> GetCarts
+        {
+            get
+            {
+                var carts = this.context.Carts
+                    .Include(cart => cart.CartItems) // get custom product
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.BaseProduct)
+                    .Include(cart => cart.CartItems) // get custom product tags
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.ProductTags)
+                    .Include(cart => cart.CartItems) // get custom pricing history
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.PricingHistory)
+                    .Include(cart => cart.CartItems) // get roster base color
+                        .ThenInclude(cartItem => cartItem.ProductSelection) 
+                            .ThenInclude(selectedProduct => selectedProduct.BaseProduct)
+                                .ThenInclude(baseProduct => baseProduct.BaseColor)
+                    .Include(cart => cart.CartItems) // get roster base size
+                        .ThenInclude(cartItem => cartItem.ProductSelection) 
+                            .ThenInclude(selectedProduct => selectedProduct.BaseProduct)
+                                .ThenInclude(baseProduct => baseProduct.BaseSize)
+                    .Include(cart => cart.CartItems) // get roster product tags
+                        .ThenInclude(cartItem => cartItem.ProductSelection) 
+                            .ThenInclude(selectedProduct => selectedProduct.BaseProduct)
+                                .ThenInclude(baseProduct => baseProduct.ProductTags)
+                    .Include(cart => cart.CartItems) // get pricing history for roster product
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.BaseProduct)
+                                .ThenInclude(baseProduct => baseProduct.PricingHistory)
+                    .ToList();
+                return carts;
+            }
+        }
+
+        public async Task<Cart> FindCartById(int cartId)
+        {
+            return this.context.Carts
+                    .Include(cart => cart.CartItems) // get custom product
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.BaseProduct)
+                    .Include(cart => cart.CartItems) // get custom product tags
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.ProductTags)
+                    .Include(cart => cart.CartItems) // get custom pricing history
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.PricingHistory)
+                    .Include(cart => cart.CartItems) // get roster base color
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.BaseProduct)
+                                .ThenInclude(baseProduct => baseProduct.BaseColor)
+                    .Include(cart => cart.CartItems) // get roster base size
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.BaseProduct)
+                                .ThenInclude(baseProduct => baseProduct.BaseSize)
+                    .Include(cart => cart.CartItems) // get roster product tags
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.BaseProduct)
+                                .ThenInclude(baseProduct => baseProduct.ProductTags)
+                    .Include(cart => cart.CartItems) // get pricing history for roster product
+                        .ThenInclude(cartItem => cartItem.ProductSelection)
+                            .ThenInclude(selectedProduct => selectedProduct.BaseProduct)
+                                .ThenInclude(baseProduct => baseProduct.PricingHistory)
+                    .ToList()
+                    .Find(cart => cart.CartID == cartId);
+        }
+    }
+}
