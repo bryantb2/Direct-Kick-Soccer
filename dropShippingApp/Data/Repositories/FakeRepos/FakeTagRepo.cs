6e4dfcc398fbaf36652c67237936b388f9b314c1
SchultzW <36714268+SchultzW@users.noreply.github.com>
2020-04-15 15:38:24 -0700

    finished building repos

---
+++ dropShippingApp/Data/Repositories/FakeRepos/FakeTagRepo.cs
@@ -0,0 +1,56 @@
+﻿using dropShippingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dropShippingApp.Data.Repositories.RealRepos
+{
+    public class FakeTagRepo : ITagRepo
+    {
+        private List<ProductTag> tags = new List<ProductTag>();
+
+        public async Task AddTag(ProductTag tag)
+        {
+            tags.Add(tag);
+        }
+
+        public async Task<ProductTag> CreateTag(string text)
+        {
+
+            ProductTag t = new ProductTag
+            {
+                TagLine = text
+            };
+            tags.Add(t);
+            return await Task.FromResult<ProductTag>(t);
+        }
+
+        public async Task<ProductTag> DeleteTag(int id)
+        {
+
+            try
+            {
+                ProductTag t = (from tag in tags
+                                where tag.ProductTagID == id
+                                select tag).First();
+                tags.Remove(t);
+                return await Task.FromResult<ProductTag>(t);
+            }
+            catch
+            {
+                return await Task.FromResult<ProductTag>(null);
+            }
+         
+        }
+
+        public async Task UpdateTag(ProductTag updatedTag)
+        {
+            ProductTag  oldTag = (from tag in tags
+                            where tag.ProductTagID == updatedTag.ProductTagID
+                            select tag).First();
+            tags.Remove(oldTag);
+            tags.Add(updatedTag);
+        }
+    }
+}
---
+++ dropShippingApp/Data/Repositories/RealRepos/RealTagRepo.cs
@@ -0,0 +1,77 @@
+﻿using dropShippingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dropShippingApp.Data.Repositories.RealRepos
+{
+    public class RealTagRepo : ITagRepo
+    {
+        private  ApplicationDbContext context;
+
+        public IQueryable<ProductTag> Tags
+        {
+            get { return context.ProductTags; }
+        }
+        public RealTagRepo(ApplicationDbContext applicationDbcontext)
+        {
+            context = applicationDbcontext;
+        }
+        public async Task AddTag(ProductTag tag)
+        {
+            await context.ProductTags.AddAsync(tag);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<ProductTag> DeleteTag(int id)
+        {
+            try
+            {
+                ProductTag t = (from tag in Tags
+                                where tag.ProductTagID == id
+                                select tag).FirstOrDefault();
+
+
+                context.ProductTags.Remove(t);
+                await context.SaveChangesAsync();
+                return await Task.FromResult<ProductTag>(t);
+            }
+            catch
+            {
+                return await Task.FromResult<ProductTag>(null);
+            }
+
+        }
+
+        public async Task<List<ProductTag>> GetAllTags()
+        {
+            List<ProductTag> tags = (from tag in Tags
+                                     select tag).ToList();
+            return await Task.FromResult<List<ProductTag>>(tags);
+        }
+
+        public async Task<ProductTag> GetTagById(int id)
+        {
+            try
+            {
+                ProductTag t = (from tag in Tags
+                                where tag.ProductTagID == id
+                                select tag).FirstOrDefault();
+                return await Task.FromResult<ProductTag>(t);
+            }
+            catch
+            {
+                return await Task.FromResult<ProductTag>(null);
+            }
+
+            
+        }
+
+        public async Task UpdateTag(ProductTag updatedTag)
+        {
+            context.Update(updatedTag);
+            await context.SaveChangesAsync();
+        }
+    }
+}
