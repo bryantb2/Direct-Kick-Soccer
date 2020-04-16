d2989489954770424a9f1b39bd25814c6be3df53
Blake <bryantb2@my.lanecc.edu>
2020-04-16 14:29:21 -0700

    fixed tag repo

--- dropShippingApp/Data/Repositories/RealRepos/RealTagRepo.cs
+++ dropShippingApp/Data/Repositories/RealRepos/RealTagRepo.cs
@@ -37,77 +37,25 @@ namespace dropShippingApp.Data.Repositories.RealRepos
             await this.context.SaveChangesAsync();
             return foundTag;
         }
-        
+
         public async Task UpdateTag(Tag tag)
         {
             this.context.Tags.Update(tag);
             await this.context.SaveChangesAsync();
-
-        private  ApplicationDbContext context;
-
-        public IQueryable<ProductTag> Tags
-        {
-            get { return context.ProductTags; }
-        }
-        public RealTagRepo(ApplicationDbContext applicationDbcontext)
-        {
-            context = applicationDbcontext;
-        }
-        public async Task AddTag(ProductTag tag)
-        {
-            await context.ProductTags.AddAsync(tag);
-            await context.SaveChangesAsync();
         }
 
-        public async Task<ProductTag> DeleteTag(int id)
+        public async Task<Tag> GetTagById(int tagId)
         {
-            try
-            {
-                ProductTag t = (from tag in Tags
-                                where tag.ProductTagID == id
-                                select tag).FirstOrDefault();
-
-
-                context.ProductTags.Remove(t);
-                await context.SaveChangesAsync();
-                return await Task.FromResult<ProductTag>(t);
-            }
-            catch
-            {
-                return await Task.FromResult<ProductTag>(null);
-            }
-
+            var foundTag = this.context.Tags.ToList()
+                .Find(tag => tag.TagID == tagId);
+            return foundTag;
         }
 
-        public async Task<List<ProductTag>> GetAllTags()
+        public async Task<Tag> GetTagByName(string name)
         {
-            List<ProductTag> tags = (from tag in Tags
-                                     select tag).ToList();
-            return await Task.FromResult<List<ProductTag>>(tags);
-        }
-
-        public async Task<ProductTag> GetTagById(int id)
-        {
-            try
-            {
-                ProductTag t = (from tag in Tags
-                                where tag.ProductTagID == id
-                                select tag).FirstOrDefault();
-                return await Task.FromResult<ProductTag>(t);
-            }
-            catch
-            {
-                return await Task.FromResult<ProductTag>(null);
-            }
-
-            
-        }
-
-        public async Task UpdateTag(ProductTag updatedTag)
-        {
-            context.Update(updatedTag);
-            await context.SaveChangesAsync();
-
+            var foundTag = this.context.Tags.ToList()
+                .Find(tag => tag.TagLine.ToUpper().Trim() == name.ToUpper().Trim());
+            return foundTag;
         }
     }
 }
