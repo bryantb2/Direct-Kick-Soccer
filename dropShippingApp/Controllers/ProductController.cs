55c6f7ac8b1e38cf8959bdef91f4a9420d16574b
SchultzW <36714268+SchultzW@users.noreply.github.com>
2020-04-22 15:57:54 -0700

    init

--- dropShippingApp/Controllers/ProductController.cs
+++ dropShippingApp/Controllers/ProductController.cs
@@ -14,11 +14,14 @@ namespace dropShippingApp.Controllers
 {
     public class ProductController : Controller
     {
-        public IRosterProductRepo Repository { get; set; }
+        public IRosterProductRepo rosterRepo { get; set; }
+        private ICustomProductRepo customRepo;
 
-        public ProductController(IRosterProductRepo repo)
+        public ProductController(IRosterProductRepo repo,ICustomProductRepo customProdRepo)
         {
-            Repository = repo;
+            rosterRepo = repo;
+            customRepo = customProdRepo;
+
         }
 
         public async Task<IActionResult> Index()
@@ -34,5 +37,52 @@ namespace dropShippingApp.Controllers
             // returns team results page 
             return View();
         }
+        [HttpGet]
+        public async Task<IActionResult> SortView()
+        {
+            List<CustomProduct> prods = (from p in customRepo.CustomProducts
+                                         select p).ToList();
+            return View(prods);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SortView(string sortOrder)
+        {
+            if (sortOrder == "AscendSort")
+            {
+                List<CustomProduct> prods = (from p in customRepo.CustomProducts
+                                             select p).ToList();
+
+                List<CustomProduct> sortedProd = prods.OrderBy(prod => prod.CurrentPrice).ToList();
+
+                return View(sortedProd);
+            }
+            else
+            {
+                List<CustomProduct> prods = (from p in customRepo.CustomProducts
+                                             select p).ToList();
+
+                List<CustomProduct> sortedProd = prods.OrderByDescending(prod => prod.CurrentPrice).ToList();
+
+                return View(sortedProd);
+            }
+
+        }
+
+        // Not currently implemented
+
+        //public ViewResult GetProductBySKU(int SKU)
+        //{
+        //    CustomProduct product = new CustomProduct();
+        //    product = repo.CustomProducts.First(p => p.SKU == SKU);
+        //    return View(product);
+        //}
+
+        //public ViewResult GetProductByModelNumber(int productNum)
+        //{
+        //    CustomProduct product = new CustomProduct();
+        //    product = repo.CustomProducts.First(p => p.ModelNumber == productNum);
+        //    return View(product);
+        //}
+
     }
 }
