f90823ca6c904b75478856e07da524f12135b7b9
SchultzW <36714268+SchultzW@users.noreply.github.com>
2020-04-28 14:28:49 -0700

    Passing off to Josh

--- dropShippingApp/Controllers/ProductController.cs
+++ dropShippingApp/Controllers/ProductController.cs
@@ -10,6 +10,7 @@ using dropShippingApp.Data;
 using dropShippingApp.Models;
 using dropShippingApp.Data.Repositories;
 using dropShippingApp.ViewModels;
+using Microsoft.Xrm.Sdk.Query;
 
 namespace dropShippingApp.Controllers
 {
@@ -17,7 +18,7 @@ namespace dropShippingApp.Controllers
     {
         private IRosterProductRepo rosterProductRepo;
         private ICustomProductRepo customProductRepo;
-        public int PageSize=30//num of prod per page
+        public int PageSize = 30;//num of prod per page
 
         public ProductController(IRosterProductRepo rosterProductRepo,
             ICustomProductRepo customProductRepo)
@@ -36,16 +37,23 @@ namespace dropShippingApp.Controllers
         public async Task<IActionResult> Search(string searchString,int productPage=1) 
         {
             var csProduct = customProductRepo.CustomProducts;
-           
-                         
+            var pagingInfo=new PagingInfoVM();
+
+
             if (!String.IsNullOrEmpty(searchString))
             {
                 csProduct = csProduct.Where(s => s.BaseProduct.Category.Name == searchString).OrderBy(p => p.CustomProductID)
-                    .Skip((productPage-1)*PageSize)
+                    .Skip((productPage - 1) * PageSize)
                     .Take(PageSize)
-                    .ToList();   
+                    .ToList();
+                pagingInfo.CurrentPge = productPage;
+                pagingInfo.ItemsPerPage = PageSize;
+                pagingInfo.TotalItems = customProductRepo.CustomProducts.Count();
+    
+
             }
-               return View(csProduct); 
+            
+               return View((csProduct,pagingInfo)); 
         }
 
         public async Task<IActionResult> PopularItems()
