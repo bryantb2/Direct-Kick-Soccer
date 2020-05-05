714075813b121e7d7c01165803aad68f7a466950
Blake <bryantb2@my.lanecc.edu>
2020-05-04 20:48:09 -0700

    working on team controller

--- dropShippingApp/Controllers/TeamController.cs
+++ dropShippingApp/Controllers/TeamController.cs
@@ -19,10 +19,17 @@ namespace dropShippingApp.Controllers
             this.teamSortRepo = sortRepo;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            return View("Search", null);
+        }
+
         public async Task<IActionResult> ViewTeam(int teamId)
         {
-            // TODO
             var team = await teamRepo.FindTeamById(teamId);
+
+            // TODO
+
             return View(team);
         }
 
@@ -74,7 +81,7 @@ namespace dropShippingApp.Controllers
                 });
         }
 
-        /*public async Task<IActionResult> Search(string searchString, int currentPage = -1)
+        public async Task<IActionResult> Search(string searchString, int currentPage = -1)
         {
             // search for teams
             var foundTeams = SearchByString(searchString);
@@ -149,36 +156,13 @@ namespace dropShippingApp.Controllers
             return View("Search", browseVM);
         }
 
-        // private actions
-        private BrowseViewModel CreateBrowseObject(List<CustomProduct> queriedProducts, int currentPageNumber, Category categoryObj = null, string searchTerm = null)
-        {
-            // setup starting and ending product indexes
-            var itemsPerPage = 30;
-            var startProduct = currentPageNumber * itemsPerPage;
-            var endProduct = startProduct + 30;
-
-            // setup paging view model
-            var pagingInfo = new BrowseViewModel()
-            {
-                Products = SplitList(queriedProducts, startProduct, endProduct),
-                CurrentPage = currentPageNumber,
-                SearchString = searchTerm == null ? null : searchTerm,
-                CurrentCategory = categoryObj == null ? null : categoryObj,
-                // next page exists if the number of products left in the query is greater than the total number of dispalyed products
-                NextPageExists = queriedProducts.Count > endProduct ? true : false,
-                PreviousPageExists = startProduct - itemsPerPage > 0 ? true : false
-            };
-
-            return pagingInfo;
-        }
-
         public async Task<ViewResult> BuildTeam(Team team)
         {
             // TODO
             // returns redirect to view team
             await teamRepo.AddTeam(team);
             return View(team);
-        }*/
+        }
 
         public async Task<ViewResult> MarkTeamInactive(int teamId)
         {
@@ -187,54 +171,6 @@ namespace dropShippingApp.Controllers
             await teamRepo.MarkInactiveById(teamId);
             return View();
         }
-
-        // only admins will have access to this
-        public async Task<ViewResult> RemoveTeam(int teamId)
-        {
-            // TODO
-            // returns redirect to browse teams
-            await teamRepo.RemoveTeam(teamId);
-            return View();
-        }
-
-        public async Task<ViewResult> BrowseTeams()
-        {
-            // TODO
-            // returns team results page 
-            var teamList = teamRepo.GetTeams;
-            return View(teamList);
-        }
-
-        public async Task<ViewResult> SearchTeams(string searchTerm)
-        {
-            // TODO
-            // returns team results page (will have view model with search term)
-            var teamList = teamRepo.GetTeams;
-            List<Team> searchResults = SearchListForMatches(teamList, searchTerm);
-            return View(searchResults);
-        }
-
-        // private stat and searching methods
-        private List<Team> SearchListForMatches(List<Team> teams, string searchTerm)
-        {
-            List<Team> searchResults = new List<Team>();
-            foreach (Team t in teams)
-            {
-                // search on team name
-                if (t.Name.ToUpper().Contains(searchTerm.ToUpper()))
-                    searchResults.Add(t);
-                foreach (Tag tag in t.TeamTags)
-                {
-                    // search on product tags
-                    if (tag.TagLine.ToUpper().Contains(searchTerm.ToUpper()))
-                        searchResults.Add(t);
-                }
-            }
-            return searchResults;
-        }
-
-        // TODO 
-        // DO NOT WRITE CODE FOR THESE UNTIL BOTH THE PRODUCT AND TEAM REPOS ARE FINISHED...
 
         public async Task<IActionResult> ManageTeamProducts()
         {
